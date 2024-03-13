beccb815e5bd19014ef6240227b31810efb4d0ad
Estrella <estrellaparra89@hotmail.com>
2024-03-13 19:10:08 +0100

    dark mode

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -31,9 +31,6 @@ dayNight.addEventListener("click", () => {
 });
 
 window.addEventListener("load", () => {
-  if (document.body.classList.contains("dark")) {
-    dayNight.querySelector("i").classList.add("fa-sun");
-  } else {
-    dayNight.querySelector("i").classList.add("fa-moon");
-  }
+  dayNight.querySelector("i").classList.add("fa-sun");
+  document.body.classList.add("dark");
 });
